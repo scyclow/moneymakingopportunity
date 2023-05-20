aa48e4a02f5dea93d1342a20a1671292de00940f
Steve Pikelny <spike716@gmail.com>
2023-05-20 18:31:25 -0400

    website

--- contracts/MMOMock.sol
+++ contracts/MMOMock.sol
@@ -8,13 +8,13 @@ pragma solidity ^0.8.17;
 
 contract MMOMock {
   uint256 public contributors = 50;
-  uint256 public ending = 0;
+  uint256 public ending = 1;
   uint256 public currentWeek = 49;
 
   constructor() {}
 
   function settlementAddressProposals(uint256 week) external view returns (address) {
-    return address(0);
+    return address(1);
   }
 
   function votes(uint256 tokenId, uint256 week) public view returns (bool) {
--- scripts/deploy.local.js
+++ scripts/deploy.local.js
@@ -1,15 +1,37 @@
 async function main() {
-  const [artist, collector1, collector2, collector3] = await ethers.getSigners()
+  const signers = await ethers.getSigners()
+  const artist = signers[0]
   console.log('Deploying base contract for artist addr:', artist.address)
 
-  TerminallyOnlineFactory = await ethers.getContractFactory('TerminallyOnline', artist)
-  TerminallyOnline = await TerminallyOnlineFactory.deploy()
-  await TerminallyOnline.deployed()
+  MoneyMakingOpportunityFactory = await ethers.getContractFactory('MoneyMakingOpportunity', artist)
+  MMO = await MoneyMakingOpportunityFactory.deploy()
+  await MMO.deployed()
+
+  const MMOTokenURIFactory = await ethers.getContractFactory('MMOTokenURI', artist)
+  MMOTokenURI = await MMOTokenURIFactory.deploy(MMO.address)
+  await MMOTokenURI.deployed()
 
 
-  console.log(`TerminallyOnline:`, TerminallyOnline.address)
-  console.log(`Multisig:`, await TerminallyOnline.connect(artist).multisig())
-  console.log(`TokenURI:`, await TerminallyOnline.connect(artist).tokenURIContract())
+  await Promise.all([
+    signers[1].sendTransaction({ to: MMO.address, value: ethers.utils.parseEther('0.03') }),
+    signers[2].sendTransaction({ to: MMO.address, value: ethers.utils.parseEther('0.03') }),
+    signers[3].sendTransaction({ to: MMO.address, value: ethers.utils.parseEther('0.03') }),
+    signers[4].sendTransaction({ to: MMO.address, value: ethers.utils.parseEther('0.03') }),
+  ])
+
+  await MMO.connect(artist).unlock(MMOTokenURI.address)
+
+  await Promise.all([
+    MMO.connect(signers[1]).claim(),
+    MMO.connect(signers[2]).claim(),
+    // MMO.connect(signers[3]).claim(),
+    MMO.connect(signers[4]).claim(),
+  ])
+
+  // await MMO.connect(signers[1]).castVote(1, 1, true)
+
+
+  console.log(`MoneyMakingOpportunity:`, MMO.address)
 }
 
 
